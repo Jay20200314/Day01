b4127cddfe26f2b033ce286c31c73d4e58c54202
Jaylen <1165399363@qq.com>
2025-01-03 15:06:16 +0800

    第一次提交 day01

---
+++ src/Demo2.java
@@ -0,0 +1,20 @@
+//第一步：创建一个Demo2的类
+//注释会被忽略掉，给程序员标注的
+/*
+* 需求：第一步:创建Demo2类
+*      第二步:编写main方法
+*               类中的函数名称必须是main，改成其他就运行不了了
+* */
+public class Demo2 {
+    public static void main(String[] args) {
+        System.out.printf("Hello Jay");
+    }
+    /*
+    * 多行注释表示的是一个区间
+    * */
+    /**
+     *
+     * 文档注释
+     */
+
+}
---
+++ src/Demo_zuoye.java
@@ -0,0 +1,7 @@
+public class Demo_zuoye {
+    public static void main(String[] args) {
+        System.out.printf("我的名字是Jay\n");
+        System.out.printf("你好世界！\n");
+        System.out.printf("我今年五岁了。\n");
+    }
+}
